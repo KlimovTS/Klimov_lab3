1714ad844bf852915330629203dd73f1fcee92d9
Timofey <klimoff.timofey3@yandex.ru>
2019-10-18 03:40:37 +0300

    added documentation and rules text

--- lab4/py.py
+++ lab4/py.py
@@ -11,17 +11,23 @@ heigth2 = 153
 WH =  str(width)+'x'+str(heigth+heigth2)
 root.geometry(WH)
 
+# основной экран
 canv = Canvas(root, width=width, height=heigth, bg='white')
 canv.pack()
+# полоса для счёта
 c = Canvas(root, width=width, height=20, bg='lightgrey')
 c.pack()
+# окно с кнопками
 d = Canvas(root, width=width, height=170, bg='white')
 d.pack()
 
-# Хранилище моих кнопок
-#Buttons = []
-# [номер функции, x0, y0, dx, dy, text, backcolor]
+# КНОПКИ:
 
+# функция кнопки():
+#      что - то
+# кнопка
+
+# перезапуск игры
 def Freset():
     global lock1, lock2, lock3, lock4, pause, startbool, playername, leaderboard, rules, prevpause
     if not (lock1 or lock2 or lock3 or lock4) and playername!='' and leaderboard==0 and rules==0:
@@ -53,6 +59,7 @@ def Freset():
                     square0t(j[1])
 reset = Button(d, text="start", command=Freset, font="Verdana 25")
 reset.grid(column=4, row=5)
+# кнопка паузы
 def Fpause():
     global pause, active
     if active==1:
@@ -63,6 +70,7 @@ def Fpause():
             Bpause.configure(text = "pause")
 Bpause = Button(d, text="pause", command=Fpause, font="Verdana 25")
 Bpause.grid(column=5, row=5)
+# кнопка открытия таблицы лидеров
 def Fleaderboard():
     global leaderboard, pause, prevpause, rules
     leaderboard = 1-leaderboard
@@ -92,6 +100,7 @@ def Fleaderboard():
         Bleaderboard.configure(text = "open leaderboard")
 Bleaderboard = Button(d, text="open leaderboard", command=Fleaderboard, font="Verdana 25")
 Bleaderboard.grid(column=6, row=5)
+# кнопка открытия правил
 def Frules():
     global  leaderboard, pause, prevpause, rules
     rules = 1-rules
@@ -119,6 +128,7 @@ def Frules():
                         square0t(j[1])
 Brules = Button(d, text="?!", command=Frules, font="Verdana 25")
 Brules.grid(column=7, row=5)
+# предохранитель перезапуска
 def Flock1():
     global lock1
     lock1 = 1-lock1
@@ -128,6 +138,7 @@ def Flock1():
         BLock1.configure(text = "unlocked")
 BLock1 = Button(d, text="locked", command=Flock1)
 BLock1.grid(column=0, row=0)
+# предохранитель перезапуска
 def Flock2():
     global lock2
     lock2 = 1-lock2
@@ -137,6 +148,7 @@ def Flock2():
         BLock2.configure(text = "unlocked")
 BLock2 = Button(d, text="locked", command=Flock2)
 BLock2.grid(column=0, row=10)
+# предохранитель перезапуска
 def Flock3():
     global lock3
     lock3 = 1-lock3
@@ -146,6 +158,7 @@ def Flock3():
         BLock3.configure(text = "unlocked")
 BLock3 = Button(d, text="locked", command=Flock3)
 BLock3.grid(column=10, row=0)
+# предохранитель перезапуска
 def Flock4():
     global lock4
     lock4 = 1-lock4
@@ -155,6 +168,7 @@ def Flock4():
         BLock4.configure(text = "unlocked")
 BLock4 = Button(d, text="locked", command=Flock4)
 BLock4.grid(column=10, row=10)
+# установка имени игрока
 def Fsetname():
     global playername
     a = e.get()
@@ -167,11 +181,11 @@ def Fsetname():
 Bsetname = Button(d, text="SET NAME   ------>", command=Fsetname)
 Bsetname.grid(column=4, row=10)
 
-
+# поле ввода имени игрока
 e = Entry(d, width=29)
 e.grid(column=5, row=10)
 
-
+# создание необходимых переменных
 playername=''
 startbool=1
 lock1=1
@@ -252,6 +266,7 @@ def start0():
     Flock4()
     active = 0
 
+# сохраняет таблицу лидеров
 def saveresult():
     global LeaderBoardText
     f1 = open('leaderboardNames.txt', 'w')
@@ -260,6 +275,7 @@ def saveresult():
         f1.write(i[0])
         f2.write(str(i[1])+'\n')
 
+# загружает таблицу лидеров
 def loadLeaderBoardText():
     global LeaderBoardText
     try:
@@ -286,12 +302,14 @@ def loadLeaderBoardText():
         A.append(LeaderBoardText[i])
     LeaderBoardText = A
 
+# выдаёт либо 1 либо -1
 def randSign():
     a = random.randint(0, 1)
     a = a*2
     a = a-1
     return a
 
+# выдаёт случайный цвет
 def randColor():
     r = 255
     g = 255
@@ -303,6 +321,7 @@ def randColor():
     col = "#%02X%02X%02X" % (r, g, b)
     return col
 
+# преобразует float в str округляя до 2 знаков после запятой
 def mstr(a):
     a=a*100
     b=int(a)
@@ -311,6 +330,7 @@ def mstr(a):
     b=b/100
     return str(b)
 
+# создаёт случайный круг
 def randCircle():
     global time1x, timeMultiplier
     x = random.randint(100,700)
@@ -323,6 +343,7 @@ def randCircle():
     k=time1x/lifeTime
     return [0, [x, y, r, color, vx, vy, lifeTime, k]]
 
+# создааёт случайный квадрат
 def randSquare():
     global time1x, timeMultiplier
     x = random.randint(100,700)
@@ -335,6 +356,7 @@ def randSquare():
     k=time1x/lifeTime
     return [1, [x, y, r, color, vx, vy, lifeTime, k]]
 
+# обрабатывает движение и столкновения круга, рисует его
 def circle(a):
     global width, heigth
     vx = a[4]
@@ -355,6 +377,7 @@ def circle(a):
     if y-r<0:
         a[5]=random.randint(1, 64)/8
 
+# рисует круг
 def circle0t(a):
     global width, heigth
     x = a[0]
@@ -363,7 +386,7 @@ def circle0t(a):
     c = a[3]
     canv.create_oval(x-r,y-r,x+r,y+r,fill = c , width=0)
     
-
+# обрабатывает движение и столкновения квадрата, рисует его
 def square(a):
     global width, heigth
     vx = a[4]
@@ -384,6 +407,7 @@ def square(a):
     if y-r<0:
         a[5]=random.randint(1, 128)/8
 
+# рисует квадрат
 def square0t(a):
     global width, heigth
     x = a[0]
@@ -392,24 +416,47 @@ def square0t(a):
     c = a[3]
     canv.create_rectangle(x+r, y+r, x-r, y-r, fill=c, width=0)
 
+# функция  для сортировки
 def mySort(a):
     return a[1]
 
+# отрисовка всего нужного
 def draw():
     global I, Objects, frameTime, width, heigth, pause, lock1, lock2, lock3,  lock4, active, leaderboard, LeaderBoardText, playername, Score, rules
-    if rules:
+    if rules: # отрисовка  правил
         canv.delete(ALL)
-        rule  = 'This is game rules'
-        canv.create_text(400, 300, text=rule, justify=CENTER, font="Verdana 30")
-    elif leaderboard:
+        rule1  = 'Правила игры' + '\n'
+        rule  = '1) Чтобы начать игру нужно задать имя.' + '\n'
+        rule  += '2) Защита от  рестарта, чтобы перезапустить игру не' + '\n'
+        rule  += 'проиграв, нужно разблокировать  4 кнопки по углам.' + '\n'
+        rule  += '3) Сохраняются только 10 лучших результатов.' + '\n'
+        rule  += '4) Подсчёт очков: количество очков за фигуру' + '\n'
+        rule  += 'зависит от площади фигуры, квадрата скорости и' + '\n'
+        rule  += 'времени жизни фигуры.' + '\n'
+        rule  += '5) Параметр COMBO показывает сколько за  раз было' + '\n'
+        rule  += 'задето фигур, все бонусы умножаются на этот параметр.' + '\n'
+        rule  += '6) Параметр DIFFICULTY отражает во сколько раз' + '\n'
+        rule  += 'меньше чем в начале живут фигурки (в начале квадрат -' + '\n'
+        rule  += 'примерно 18 сек, круг - 12 сек).' + '\n'
+        rule  += '7)Параметр RADIUS BONUS показывает какой будет радиус' + '\n'
+        rule  += 'курсора, это упрощает попадание, за промах' + '\n'
+        rule  += 'снимается 4 очка, за попаданет добавляется 1 очко.' + '\n'
+        rule  += 'При RADIUS BONUS  --> бесконечность - радиус курсора' + '\n'
+        rule  += 'примерно равен максимально возможному кругу' + '\n'
+        rule  += '(R ~ 1 - 0,99^RADIUS BONUS).' + '\n'
+        rule  += '8) Игра кончаеся когда не остаётся фигур, при' + '\n'
+        rule  += 'попадании  появляется 2 фигуры.' + '\n'
+        canv.create_text(400, 60, text=rule1, justify=CENTER, font="Verdana 30")
+        canv.create_text(400, 330, text=rule, justify=CENTER, font="Verdana 15")
+    elif leaderboard: # отрисовка таблицы лидеров
         canv.delete(ALL)
         for i in range(0, 10, 1):
             txt = str(i+1) + '     ' + LeaderBoardText[i][0][:-1] + ' = ' + mstr(LeaderBoardText[i][1]) + ' Points'
             canv.create_text(30, 30+60*i, text=txt, justify=CENTER, font="Verdana 30", anchor=W)
     else:
         if pause==0:
-            if len(Objects)==0:
-                if active:
+            if len(Objects)==0: # условие конца игры
+                if active: # условие для однократного срабатывания конца игры
                     canv.delete(ALL)
                     canv.create_text(width/2, heigth/2, text="Game over!", justify=CENTER, font="Verdana 70")
                     lock1=1
@@ -428,27 +475,29 @@ def draw():
                     LeaderBoardText = A
                     saveresult()
                     active = 0
-            else:
+            else: # отрисовка  игры
                 canv.delete(ALL)
                 c.delete(ALL)
                 c.create_text(30, 10, text=I, justify=CENTER, font="Verdana 14", anchor=W)
                 c.create_text(770, 10, text=Difficulty, justify=CENTER, font="Verdana 14", anchor=E)
+                # обработка движения
                 for j in Objects:
                     if j[0]==0:
                         circle(j[1])
                     if j[0]==1:
                         square(j[1])
-                for j in Objects:
+                # обработка времени жизни объектов
+                for j in Objects: 
+                    j[1][6]=j[1][6]-frameTime
                     if j[1][6]<0:
                         Objects.remove(j)
-                    else:
-                        j[1][6]=j[1][6]-frameTime
+                        
     
     
     
     root.after(frameTime, draw)
 
-
+# обработка кликов
 def click(event):
     global Score, I, Objects, dI, counter, timeMultiplier,  Difficulty, pause, clickStreak, rules, leaderboard, MouseRadius, active
     if pause==0 and active==1:
@@ -459,7 +508,7 @@ def click(event):
         tmpR=50-MouseRadius
         for j in Objects:
             tmp = 1
-            if j[0]==0:
+            if j[0]==0:  # обработка объекта если это круг
                 if (event.x-j[1][0])*(event.x-j[1][0])+(event.y-j[1][1])*(event.y-j[1][1])<((j[1][2]+tmpR)*(j[1][2]+tmpR)):
                     counter+=1
                     k1=(16+j[1][4]*j[1][4]+j[1][5]*j[1][5])/16
@@ -485,7 +534,7 @@ def click(event):
                         obj.append(randSquare())
                         obj.append(randSquare())
                     tmp = 0
-            if j[0]==1:
+            if j[0]==1: # обработка объекта если это квадрат
                 if (((event.x>j[1][0]-j[1][2]-tmpR) and (event.x<j[1][0]+j[1][2]+tmpR)) and ((event.y>j[1][1]-j[1][2]-tmpR) and (event.y<j[1][1]+j[1][2]+tmpR))):
                     counter+=1
                     k1=(16+j[1][4]*j[1][4]+j[1][5]*j[1][5])/16
@@ -511,9 +560,9 @@ def click(event):
                         obj.append(randCircle())
                         obj.append(randCircle())
                     tmp = 0
-            if tmp:
+            if tmp: # оставляет объекты по которым небыло попадания
                 obj.append(j)
-            else:
+            else: # усложнение при попадании
                 timeMultiplier = timeMultiplier*0.9931
         Objects=obj
         ss = 0
@@ -537,6 +586,7 @@ def click(event):
                 MouseRadius=MouseRadius*(0.99**(len(s)*combo))
                 canv.create_oval(event.x+tmpR, event.y+tmpR, event.x-tmpR, event.y-tmpR, fill = "black" , width=0)
         Score+=dI
+        # изменение выводимого текста счётчиков
         Difficulty='difficulty='+mstr(2/timeMultiplier)+'   radius bonus='+str(clickStreak)
         if combo>0:
             if combo>1:
@@ -544,7 +594,7 @@ def click(event):
             else:
                 I=mstr(Score)+' +'+mstr(dI)
         else:
-            I = I
+            I = mstr(Score)
 
 
 start0()
